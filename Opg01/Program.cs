df532e5d5afe99812037979cea46dc802a6ea063
lefalk <lfa@bankdata.dk>
2017-04-27 15:01:21 +0200

    Start

---
+++ Opg01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast navn");
+            string navn = Console.ReadLine();
+            string stortNavn = navn.ToUpper();
+            Console.WriteLine("Velkommen" + stortNavn);
+            Console.WriteLine("Tryk på en tast for at fortsætte");
+            Console.ReadKey();
+
+        }
+    }
+}
