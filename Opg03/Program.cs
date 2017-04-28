6f2dae8ab3d2dd2193b5b796410004c1b9a394fa
lefalk <lfa@bankdata.dk>
2017-04-28 13:23:39 +0200

    Månedsløn beregning

--- Opg03/Program.cs
+++ Opg03/Program.cs
@@ -10,6 +10,24 @@ namespace Opg03
     {
         static void Main(string[] args)
         {
+
+            int[] månedsløn = { 1000, 2000, 15000, 200000, 30000, 15000 };
+            double sum = 0;
+            for (int i = 0; i < månedsløn.Length; i++)
+
+            {
+                sum += månedsløn[i];
+            }
+
+            double gns = sum / månedsløn.Length;
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                Console.WriteLine(månedsløn[i]);
+
+            }
+
+            Console.WriteLine("Gennemsnit" +" " +gns.ToString("N2"));
         }
     }
 }
