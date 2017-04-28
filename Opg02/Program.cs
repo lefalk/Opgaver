0bec875dfe4304d2c50ae46ea8b50d563ee55c20
lefalk <lfa@bankdata.dk>
2017-04-28 10:58:01 +0200

    Opgave 2 færdig

--- Opg02/Program.cs
+++ Opg02/Program.cs
@@ -10,6 +10,56 @@ namespace Opg02
     {
         static void Main(string[] args)
         {
+
+            int heltal = 10;
+            heltal++;
+            heltal--;
+            heltal+= 20;
+            Console.WriteLine(heltal);
+            
+            double kommatal = 12.5;
+            kommatal++;
+            kommatal--;
+            kommatal *= 2;
+            Console.WriteLine(kommatal);
+
+            Filtyper ft = Filtyper.csv;
+            Console.WriteLine(ft);
+            Console.WriteLine((int)ft);
+
+            DateTime dateTime = DateTime.Now;
+            Console.WriteLine(dateTime.ToString("ddMMyy"));
+            Console.WriteLine(dateTime.ToLongTimeString());
+            Console.WriteLine(dateTime.ToString("d-m-y"));
+
+            Person p;
+            p.ID = 1;
+            p.Navn = "Mikkel";
+            Console.WriteLine(p.Navn);
+
+
+
+
+
+
+
+
         }
+    }
+
+    public struct Person
+
+    {
+
+      public int ID;
+      public string Navn;
+    }
+    enum Filtyper
+
+    {
+
+        pdf,
+        txt,
+        csv
     }
 }
