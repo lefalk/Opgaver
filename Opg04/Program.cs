1e2f814af8f885f7db1b6a91d8d0223fc0f397b6
lefalk <lfa@bankdata.dk>
2017-04-28 15:24:27 +0200

    Opg4

---
+++ Opg04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fornavn = ("Mikkel");
+            String efternavn = ("Cronberg");
+
+            String navn = (fornavn +" " + efternavn);
+            String stortnavn = navn.ToUpper();
+            String lillenavn = navn.ToLower();
+            String delnavn = lillenavn.Substring(7,4);
+
+            String[] navne = navn.Split(' ');
+            foreach (var item in navn)
+       
+
+            Console.WriteLine(navn);
+            Console.WriteLine(stortnavn);
+            Console.WriteLine(lillenavn);
+            Console.WriteLine(delnavn);
+          
+
+
+
+
+
+        }
+    }
+}
